7b2b0937282c8213b34277bc89ccb42233f5e8a8
rferdinandjuko <rferdinandjuko@gmail.com>
2025-02-05 16:10:11 +0100

    top -100% is over top; active is choosing depend registration or login

---
+++ login-and-registration-form/script.js
@@ -0,0 +1,6 @@
+let toggleForm = () => {
+    section = document.querySelector('section');
+    container = document.querySelector('.container');
+    container.classList.toggle('active');
+    section.classList.toggle('active');
+}
