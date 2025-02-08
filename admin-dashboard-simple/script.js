9d50e587aeb6d2be0fdd71e973eb5ee1b02b59b1
rferdinandjuko <rferdinandjuko@gmail.com>
2025-02-08 21:48:20 +0100

    chart js configuration

--- admin-dashboard-simple/script.js
+++ admin-dashboard-simple/script.js
@@ -5,7 +5,7 @@ let main = document.querySelector('.main');
 
     toggle.onclick = function() {
         navigation.classList.toggle('active');
-        main.classList.toggle('main')
+        main.classList.toggle('active')
     }
 
 
@@ -19,3 +19,54 @@ function activeLink() {
 
 list.forEach((item) =>
     item.addEventListener('mouseover', activeLink))
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var earning = document.getElementById('earning').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['Facebook', 'Youtube', 'Amazon'],
+        datasets: [{
+            label: 'Traffic Source',
+            data: [1200, 1900, 3000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
+
+
+var myChart = new Chart(earning, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'Mey', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: 'Earning',
+            data: [1200, 1090, 3000, 5400, 1220, 3600, 1200, 54000, 4578, 2566, 8545,4587,  9000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
