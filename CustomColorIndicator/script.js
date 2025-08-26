a6c924f8827732764f91905cbb01bbbbdfdddfe9
rferdinandjuko <rferdinandjuko@gmail.com>
2025-08-26 10:08:57 +0200

    Utilisation de l'événement passé en paramètre: e.currentTarget.classList.add('active')

---
+++ CustomColorIndicator/script.js
@@ -0,0 +1,10 @@
+const list = document.querySelectorAll(".list");
+let activeLink = (e) => {
+    list.forEach((item) => {
+        item.classList.remove("active");
+        e.currentTarget.classList.add("active");
+    })
+}
+
+list.forEach((item) =>
+    item.onclick = activeLink);
