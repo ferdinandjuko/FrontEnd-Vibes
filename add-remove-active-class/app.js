dc9da8da1aea3ffcda4e91ce0cef3e3fa9f49f01
rferdinandjuko <rferdinandjuko@gmail.com>
2025-09-12 19:33:02 +0200

    Color of text #262626 become bg of li.active on click cause of toggle

--- add-remove-active-class/app.js
+++ add-remove-active-class/app.js
@@ -1,8 +1,9 @@
-var liste = document.querySelectorAll('li');
+/* Color of text #262626 become bg of li.active on click cause of toggle */
+let liste = document.querySelectorAll('li');
 liste.forEach(element => {
-    element.addEventListener('click', function() {
+    element.onclick = () => {
         let activeElement = document.querySelector('li.active');
         activeElement.classList.remove("active");
-        element.classList.add("active")
-    })
+        element.classList.add("active");
+    }
 });
