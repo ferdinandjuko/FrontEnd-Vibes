26799cea689825e674387315631f09c0d63972c3
rferdinandjuko <rferdinandjuko@gmail.com>
2025-09-14 19:33:12 +0200

    Date().getHours; Date().getMinutes(); Date().getSeconds

--- analog-and-digital-clock/script.js
+++ analog-and-digital-clock/script.js
@@ -1,21 +1,23 @@
-let hr = document.querySelector('#hr')
-let mn = document.querySelector('#mn')
-let sc = document.querySelector('#sc')
+// Date().getHours; Date().getMinutes(); Date().getSeconds
+let hr = document.getElementById("hr");
+let mn = document.getElementById("mn");
+let sc = document.getElementById("sc");
 
-setInterval( () => {
+setInterval(() => {
     let day = new Date();
     let hh = day.getHours() * 30;
     let mm = day.getMinutes() * 6;
     let ss = day.getSeconds() * 6;
-    
-    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
     mn.style.transform = `rotateZ(${mm}deg)`;
     sc.style.transform = `rotateZ(${ss}deg)`;
-    // Digital Clock
-    let hours = document.getElementById('hours');
-    let minutes = document.getElementById('minutes');
-    let seconds = document.getElementById('seconds');
-    let ampm = document.getElementById('ampm');
+
+    // Digital clock
+    let hours = document.getElementById("hours");
+    let minutes = document.getElementById("minutes");
+    let seconds = document.getElementById("seconds");
+    let ampm = document.getElementById("ampm");
 
     let h = new Date().getHours();
     let m = new Date().getMinutes();
@@ -28,18 +30,13 @@ setInterval( () => {
         h = h - 12;
     }
 
-    // add zero before signle digit number
-    h = (h < 10) ? "0" + h : h
-    m = (m < 10) ? "0" + m : m
-    s = (s < 10) ? "0" + s : s
+    // add zero before single digit number
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
 
     hours.innerHTML = h;
     minutes.innerHTML = m;
     seconds.innerHTML = s;
     ampm.innerHTML = am;
 })
-
-
-
-
-
