5a1425128de28f21a70c1549a52be4482e08e6b5
rferdinandjuko <rferdinandjuko@gmail.com>
2025-09-20 21:04:58 +0200

    Implement glassmorphism effect with backdrop-filter

---
+++ bgTransparencyRgba255/script.js
@@ -0,0 +1,10 @@
+VanillaTilt.init(document.querySelectorAll(".card")
+    , {
+        max: 25,
+        speed: 400,
+        glare: true,
+        "max-glare": 1,
+    });
+
+//It also supports NodeList
+// VanillaTilt.init(document.querySelectorAll(".your-element"));
