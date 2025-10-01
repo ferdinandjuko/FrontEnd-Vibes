47386c64ed61ad34a351f2cd6d08536679b54065
rferdinandjuko <rferdinandjuko@gmail.com>
2025-10-01 16:11:59 +0200

    backface-visibility = Clean 3D Rotations The 3D flip formula:      1. preserve-3d (enable 3D space)      2. transform-origin (set rotation axis)      3. backface-visibility: hidden (hide inactive face)      4. Pre-rotate back element 180deg Mental model: Book flip = origin + backface control Perfect 3D achieved

---
+++ backfaceVisibility/script.js
@@ -0,0 +1,5 @@
+/* backface-visibility = Clean 3D Rotations The 3D flip formula:
+      1. preserve-3d (enable 3D space)
+      2. transform-origin (set rotation axis)
+      3. backface-visibility: hidden (hide inactive face)
+      4. Pre-rotate back element 180deg Mental model: Book flip = origin + backface control Perfect 3D achieved */
