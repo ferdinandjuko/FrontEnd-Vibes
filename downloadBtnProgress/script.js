b61e6cbbdb6bd7f25fe7755779f28ebf285e805b
rferdinandjuko <rferdinandjuko@gmail.com>
2025-11-25 11:44:07 +0100

    setInterval function to increment degree value on 100ms to set progerssion feature

--- downloadBtnProgress/script.js
+++ downloadBtnProgress/script.js
@@ -4,12 +4,15 @@ let mumber = document.getElementById('mumber');
 
 download.onclick = function (event) {
     download.classList.add('start');
-    let degree = 0;
-    let interval = setInterval(() => {
+    var degree = 0;
+    let interval = setInterval(function () {
         degree += 1;
         if (degree > 100) {
             clearInterval(interval);
-            number.innerHTML = '<i class="fa-solid fa-check"></i>';
+            number.innerHTML = '<i class="fa-solid fa-check"></i><br><small>100% Done</small>';
+            return;
         }
-    });
+        container.style.background = `conic-gradient(#f5036c ${degree}%, #333 0%)`;
+        number.innerHTML = degree + '<span>%</span>'
+    }, 100);
 }
