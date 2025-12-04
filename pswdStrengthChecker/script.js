41c632eccbfa0837459689b36090f69ad2d8ff7a
rferdinandjuko <rferdinandjuko@gmail.com>
2025-12-04 22:05:30 +0300

    toggle content of .show onclick event

--- pswdStrengthChecker/script.js
+++ pswdStrengthChecker/script.js
@@ -1,3 +1,22 @@
+function strength(password) {
+    let i = 0;
+    if (password.length > 6) {
+        i++;
+    }
+    if (password.length >= 10) {
+        i++;
+    }
+    if (/[A-Z]/.test(password)) {
+        i++;
+    }
+    if (/[a-z]/.test(password)) {
+        i++;
+    }
+    if (/[0-9]/.test(password)) {
+        i++;
+    }
+}
+
 let pswrd = document.querySelector('#myPassword');
 let show = document.querySelector('.show');
 show.onclick = function () {
@@ -6,5 +25,6 @@ show.onclick = function () {
         show.classList.add('hide');
     } else {
         pswrd.setAttribute('type', 'password');
+        show.classList.remove('hide');
     }
 }
